14c7ea6d31e4a7cff18884fe19a5e1c6012e6197
shiimaxx <yoshima@shiimaxx.com>
2021-10-02 16:17:06 +0900

    systemd-journaldの負荷が高いのでログ出力を減らす

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -180,7 +180,7 @@ func serveMux() http.Handler {
 
 	router.PathPrefix("/").HandlerFunc(htmlHandler)
 
-	return logger(router)
+	return router
 }
 
 func logger(handler http.Handler) http.Handler {
